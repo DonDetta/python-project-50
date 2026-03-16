1401039733eb57fe11518d100e81aa210026948a
graphplaying <diwhiteps@list.ru>
2026-03-16 14:08:39 +0300

    part 4

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/DonDetta/python-project-50/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/DonDetta/python-project-50/actions)
+
+## Gendiff
+
+Утилита для сравнения двух конфигурационных файлов.
+
+[![asciicast](https://asciinema.org/a/cqLVfzMlNDobbRqR.svg)](https://asciinema.org/a/cqLVfzMlNDobbRqR)
--- gendiff/__init__.py
+++ gendiff/__init__.py
@@ -1,6 +1,30 @@
 import json
 
 
+def format_value(value):
+    if isinstance(value, bool):
+        return str(value).lower()
+    if value is None:
+        return 'null'
+    return str(value)
+
+
 def generate_diff(file_path1, file_path2):
     data1 = json.load(open(file_path1))
     data2 = json.load(open(file_path2))
+
+    keys = sorted(data1.keys() | data2.keys())
+    lines = []
+
+    for key in keys:
+        if key not in data2:
+            lines.append(f'  - {key}: {format_value(data1[key])}')
+        elif key not in data1:
+            lines.append(f'  + {key}: {format_value(data2[key])}')
+        elif data1[key] == data2[key]:
+            lines.append(f'    {key}: {format_value(data1[key])}')
+        else:
+            lines.append(f'  - {key}: {format_value(data1[key])}')
+            lines.append(f'  + {key}: {format_value(data2[key])}')
+
+    return '{\n' + '\n'.join(lines) + '\n}'
